66ce63c39837a3ac32df1b0ff0ac518233d3df21
Peerapol Pocharoen <6510615252@cn330>
2024-10-03 21:09:21 +0700

    test 1

--- Mywebsite/admin.py
+++ Mywebsite/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from Mywebsite.models import Quota
 # Register your models here.
+
+admin.site.register(Quota)
--- Mywebsite/views.py
+++ Mywebsite/views.py
@@ -1,11 +1,28 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from Mywebsite.models import Quota
 
 # Create your views here.
 def index(request):
-    return render(request,"main.html")
+    all_subject = Quota.objects.all()   
+    return render(request,"main.html",{"all_subject":all_subject})
 
 def history(request):
+    if request.method == "GET":
+            Subject = request.GET["subject.Subject"]
+            Year = request.GET["subject.Year"]
+            Semester = request.GET["subject.Semester"]
+            Slot = request.GET["subject.Slot"]
+            Status = request.GET["subject.Status"]
+
+            Quota = Quota.objects.create(
+                Subject = subject,
+                Year = Year,
+                Semester = Semester,
+                Slot = Slot,
+                Status = Status
+            )
+            Quota.save()
     return render(request,"history.html")
 
 def request(request):
