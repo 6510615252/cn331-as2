511f25a12cf8289b557ccc5673ff56d3a46426f9
Peerapol Pocharoen <6510615252@cn330>
2024-10-04 15:53:26 +0700

    login logout done

--- Mywebsite/urls.py
+++ Mywebsite/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from Mywebsite import views
+from django.contrib.auth import views as auth_views
 urlpatterns = [
-    path('', views.index),
-    path('history/', views.history),
+    path('', views.login_view, name = "login"),
+    path('login/', views.login_view, name = "login"),
     path('request/', views.request),
+    path('main/', views.index, name = "main"),
+    path('register/', views.register),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
--- Mywebsite/views.py
+++ Mywebsite/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from Mywebsite.models import Quota
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 def index(request):
@@ -8,22 +12,49 @@ def index(request):
     return render(request,"main.html",{"all_subject":all_subject})
 
 def history(request):
-    if request.method == "GET":
-            Subject = request.GET["subject.Subject"]
-            Year = request.GET["subject.Year"]
-            Semester = request.GET["subject.Semester"]
-            Slot = request.GET["subject.Slot"]
-            Status = request.GET["subject.Status"]
+    # if request.method == "GET":
+    #         Subject = request.GET["subject.Subject"]
+    #         Year = request.GET["Year"]
+    #         Semester = request.GET["Semester"]
+    #         Slot = request.GET["Slot"]
+    #         Status = request.GET["Status"]
 
-            Quota = Quota.objects.create(
-                Subject = subject,
-                Year = Year,
-                Semester = Semester,
-                Slot = Slot,
-                Status = Status
-            )
-            Quota.save()
+    #         Quota = Quota.objects.create(
+    #             Subject = Subject,
+    #             Year = Year,
+    #             Semester = Semester,
+    #             Slot = Slot,
+    #             Status = Status
+    #         )
+    #     Quota.save()
     return render(request,"history.html")
 
-def request(request):
-    return render(request,"request.html")
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username = username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('main')
+        else:
+            messages.error(request, "Invalid username or password.")
+    return render(request, "login.html")
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        confirm_password = request.POST["confirm_password"]
+        
+        if password == confirm_password:
+            try:
+                user = User.objects.create_user(username=username, password=password)
+                messages.success(request, "Registration successful. You can now log in.")
+                return redirect('login')
+            except Exception as e:
+                messages.error(request, f"An error occurred: {str(e)}")
+        else:
+            messages.error(request, "Passwords do not match.")
+    
+    return render(request, "register.html")
