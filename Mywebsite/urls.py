544b704fdf04d7cac46b0c7e30a140b41e011107
Peerapol Pocharoen <6510615252@cn330>
2024-10-04 16:03:14 +0700

    login logout done

--- Mywebsite/urls.py
+++ Mywebsite/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.login_view, name = "login"),
     path('login/', views.login_view, name = "login"),
-    path('request/', views.request),
+    path('history/', views.history),
     path('main/', views.index, name = "main"),
     path('register/', views.register),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
